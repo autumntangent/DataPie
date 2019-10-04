f4162e909f00c76e52435eb8b88960b029532baf
A <hexywitch@mail.com>
2019-10-03 22:06:49 -0400

    Update config.py

--- config.py
+++ config.py
@@ -9,3 +9,21 @@ API_KEYS = {
 "url_scan":""
 
 }
+
+#PLACING LISTS & DICTS IN THE CONFIG FILE INSTEAD OF THE MAIN SOURCE CODE 
+
+SUBDOMAINS = [
+"api.", "developers.","developer.","securelogin.","console.", "db.", "careers." ]
+
+
+PAGES  = ["/", "/index.html", "/admin.php", "/login.php","/login.html", 
+"/auth/login", "/oauth2/authorize", "/crossdomain.xml", "/signin", "/admin.html",
+"/auth", "/auth/sign_in", "/auth.db", "/auth/signin", "/forgotpassword", 
+"/securelogin.asp", "/changepassword.php", "/resetpassword.php", "/password_reset", 
+"/api", "/resetpassword", "/mysql", "/mysql.db", "/.db", "/console"]
+
+BASE_ENDPOINTS = {
+
+"st":"https://api.securitytrails.com"	
+
+}
