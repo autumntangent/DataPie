8c28b0a9bb0f15aebbf47b8bb3a715d655a3537c
Autumn Tangent <52340269+autumntangent@users.noreply.github.com>
2019-10-28 04:15:08 +0000

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,26 @@ API_KEYS = {
 "sectrails":"",
 "shodan_key":"",
 "full_contact":"",
-"url_scan":""
+"hunter":"",
+"virus_total":""
+
+}
+
+#DICTS AND LISTS THAT WILL BE USED THROUGHOUT THE PROGRAM
+
+SUBDOMAINS = [
+"api.", "developers.","developer.","securelogin.",
+"console.", "db.", "careers.", "intranet." ]
+
+
+PAGES  = ["/", "/index.html", "/admin.php", "/login.php","/login.html", 
+"/auth/login", "/oauth2/authorize", "/crossdomain.xml", "/signin", "/admin.html",
+"/auth", "/auth/sign_in", "/auth.db", "/auth/signin", "/forgotpassword", 
+"/securelogin.asp", "/changepassword.php", "/resetpassword.php", "/password_reset", 
+"/api", "/resetpassword", "/mysql", "/mysql.db", "/.db", "/console"]
+
+BASE_ENDPOINTS = {
+
+"st":"https://api.securitytrails.com"	
 
 }
